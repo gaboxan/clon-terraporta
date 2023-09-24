13728cf660268ee6aa8f302eedfbd7c5f2ed322d
gaboxan <82128741+gaboxan@users.noreply.github.com>
2023-09-24 14:35:22 -0300

    v1

--- src/app/propiedad-detalles/propiedad-detalles.component.ts
+++ src/app/propiedad-detalles/propiedad-detalles.component.ts
@@ -11,10 +11,10 @@ export class PropiedadDetallesComponent {
   detalles = [
     {
       avatar: '../../assets/img/agentes/2.jpg',
-      image: 'https://www.terraporta.cl/Docs/Empresa/101/GCv_329115521770.png',
-      id:'1',
-      fono: '+569 99772206',
-      correo: 'nataliarios@terraporta.cl',
+      image: '../../assets/img/agentes/cristina_avatar.png',
+      id: '1',
+      fono: '+569 xxxxxxxx',
+      correo: 'nataliarios@correo.cl',
       tipoterreno: 'Sup. terreno',
       titulo: 'Sector El Huertón, Camino Antuco',
       banos: 0,
@@ -25,13 +25,15 @@ export class PropiedadDetallesComponent {
       agente: 'Natalia Ríos',
       descripcion:
         'Camino Antuco a pocos minutos de la ciudad, se encuentran estas hermosas parcelas de 5000m2. Los sitios son parte de un proyecto de subdivisión con rol asignado, cuentan con camino y luz soterrada. A solo 5 km de Los Ángeles',
+        profile: 'https://d1cfu8v5n1wsm.cloudfront.net/toctoc/fotos/20180813/1066252/n_wm_2019080218230270280.webp'
+
     },
     {
       avatar: '../../assets/img/agentes/1.jpg',
-      image: 'https://www.terraporta.cl/Docs/Empresa/101/GCv_356153021092.png',
-      id:'2',
-      fono: '+569 76143615',
-      correo: 'consueloarias@terraporta.cl',
+      image: '../../assets/img/agentes/dana_avatar.png',
+      id: '2',
+      fono: '+569 xxxxxxxx',
+      correo: 'consueloarias@correo.cl',
       tipoterreno: 'Sup. construidos',
       titulo: 'Casa Nueva, Condominio Bosque Laguna',
       habitaciones: 3,
@@ -42,13 +44,14 @@ export class PropiedadDetallesComponent {
       agente: 'Consuelo Arias',
       descripcion:
         'Camino Antuco a pocos minutos de la ciudad, se encuentran estas hermosas parcelas de 5000m2. Los sitios son parte de un proyecto de subdivisión con rol asignado, cuentan con camino y luz soterrada. A solo 5 km de Los Ángeles',
+        profile: 'https://cache.enlaceinmobiliario.cl/EBBcarruselcollaocollao-63358e74b5e9b4.webp'
     },
     {
       avatar: '../../assets/img/agentes/1.jpg',
-      image: 'https://www.terraporta.cl/Docs/Empresa/101/GCv_356153021092.png',
-      id:'3',
-      fono: '+569 76143615',
-      correo: 'consueloarias@terraporta.cl',
+      image: '../../assets/img/agentes/dana_avatar.png',
+      id: '3',
+      fono: '+569 xxxxxxxx',
+      correo: 'consueloarias@correo.cl',
       tipoterreno: 'Sup. construidos',
       titulo: 'Condominio Granja Osiris',
       habitaciones: 5,
@@ -59,33 +62,31 @@ export class PropiedadDetallesComponent {
       agente: 'Consuelo Arias',
       descripcion:
         'Vivienda en condominio con acceso controlado las 24 horas. A pocos minutos del centro de la ciudad.',
+      profile:
+        'https://www.brioinmobiliaria.cl/wp-content/uploads/2020/08/CASA-C-1-2-1536x864.jpg',
     },
   ];
-  showOthers:any=[]
-  constructor(private route: ActivatedRoute, private router:Router) {}
+  showOthers: any = [];
+  constructor(private route: ActivatedRoute, private router: Router) {}
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
       this.propertyId = params['propertyid'];
       const propertyIdS = this.propertyId.toString();
-  
-     
+
       const detallesCopy = [...this.detalles];
-  
+
       this.detalles = detallesCopy.filter(
         (detalle) => detalle.id === propertyIdS
       );
       this.showOthers = detallesCopy.filter(
         (detalle) => detalle.id !== propertyIdS
       );
-  
+
       console.log(this.detalles, this.propertyId, 'otras: ', this.showOthers);
     });
   }
-  goDetails(propertyid:string){
+  goDetails(propertyid: string) {
     const url = `/propiedadDetalles?propertyid=${propertyid}`;
     window.open(url, '_blank');
   }
-  
-
-  }
-
+}
